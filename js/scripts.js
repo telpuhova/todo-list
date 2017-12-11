8faa9460e31ebe925eb42d81d077c6c2b1e46441
Guest User <Guest@Epicodus-10E.local>
2017-12-11 14:56:38 -0800

    add description of items

--- js/scripts.js
+++ js/scripts.js
@@ -6,56 +6,45 @@ var Thing = function(title, description, isChecked, group) {
   this.group = group;
 }
 
-Thing.prototype.doalert = function(elem) {
-  // debugger;
-  alert(elem.title);
-}
 
 //ui
 $(document).ready(function() {
   $("#formTodo").submit(function() {
     event.preventDefault();
-    // debugger;
 
     var title = $("#title").val();
     var description = $("#description").val();
     var isChecked = false;
     var group = $("#group").val();
-    //$("input:checkbox[name=work-transport]:checked")
 
     var newThing = new Thing(title, description, isChecked, group);
 
-    $("#list").append("<p id='" + newThing.title + "P'><input id='" + newThing.title + "' type='checkbox' name='first' value='" + newThing.title + "'>" + newThing.title + "</p>");
-    // <input type="checkbox" name="first" value="thing">thing<br>
+    // add new checkbox item to the list
+    $("#list").append("<p id='" + newThing.title + "P'><input id='" + newThing.title + "' type='checkbox' name='first' value='" + newThing.title + "'>" + "<span id='" + newThing.title + "Span'>" + newThing.title + "</span>" + "</p>");
 
-    // var a = $("input:checkbox[name=first]");
-    // alert($("input:checkbox[name=first]").checked);
-
-    // input.addEventListener('change', doalert, false);
-    // document.getElementById(newThing.title).addEventListener('change', newThing.doalert(newThing), false);
+    // on changing: item gets crossed and goes down
     document.getElementById(newThing.title).addEventListener('change', function() {
-      alert(newThing.title);
-      // $("#hhh").css('text-decoration', 'line-through');
+      // alert(newThing.title);
       $("#" + newThing.title + "P").css('text-decoration', 'line-through');
-      // $("." + newThing.title + "P").hide();
-      // $("#" + newThing.title + "P").addClass(".crossedOut");
-      // $(this).insertAfter($(this).parent().find('.name'));
       $("#" + newThing.title + "P").insertAfter($("#list").last());
+    })
 
-
+    // crossed items get cleared
     $("#clear").click(function() {
       $("input:checkbox[name=first]:checked").each(function(){
-        // alert($(this).val())
         $(this).parent().hide();
       });
     });
 
+    // description becomes visible
+    $("#" + newThing.title + "Span").click(function() {
+      $(".descriptionDisplay").show();
+      $(".descriptionDisplay").text(newThing.description);
+      $(".descriptionDisplay").click(function() {
+        $(".descriptionDisplay").hide();
+      });
     });
 
 
-    // if (document.getElementById(newThing.title).checked === true)
   });
-  // document.addEventListener('change', function() {
-  //   alert("123123");
-  // });
 });
